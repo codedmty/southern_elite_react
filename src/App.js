33481d0ffb8f56c51ac66b794965cc16cc03dcd3
eddiefrs <eddie@flexrentalsolutions.com>
2019-06-29 13:04:09 -0500

    First draft of the Southern Elite react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img className="logo" src={logo} alt="logo goes here" />
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a
+            className="contact"
+            href="mailto:erikhudson1@gmail.com"
+            target="blank"
+          >
+            Contact Us
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
